38e36910c666513ab970d557db5338760f2cfa4f
CelienFio <celien.fiorelli@ynov.com>
2023-11-24 09:41:19 +0100

    add color picker tool

--- back/server.js
+++ back/server.js
@@ -25,34 +25,33 @@ ws.on('connection', socket => {
         socket.send(JSON.stringify({ action: "init", data: await Pixel.find() }))
     })();
 
-socket.on('message', m => {
-    const { action, data } = JSON.parse(m);
+    socket.on('message', m => {
+        const { action, data } = JSON.parse(m);
 
-    if (["draw", "msg"].includes(action)) {
-        ws.clients.forEach(client => {
-            if (client.readyState == webSocket.OPEN)
-                client.send(JSON.stringify({ action, data }))
-        })
-    }
-    if (action === "draw") {
-        (async () => {
-            const pixel = await Pixel.findOne({x: data.x, y: data.y})
-            if (pixel) {
-                pixel.color = data.color;
-                pixel.save()
-            } else {
-                Pixel.create({ color: data.color, x: data.x, y: data.y })
-            }
-        })()
-    }
-    if (action === "join") {
-        ws.clients.forEach(client => {
-            if (client.readyState == webSocket.OPEN)
-                client.send(JSON.stringify({ action: "msg", data: { author: data.pseudo, content: "viens de se connecter !", type: 2 }}))
-        })
-    }
-});
-
+        if (["draw", "msg"].includes(action)) {
+            ws.clients.forEach(client => {
+                if (client.readyState == webSocket.OPEN)
+                    client.send(JSON.stringify({ action, data }))
+            })
+        }
+        if (action === "draw") {
+            (async () => {
+                const pixel = await Pixel.findOne({ x: data.x, y: data.y })
+                if (pixel) {
+                    pixel.color = data.color;
+                    pixel.save()
+                } else {
+                    Pixel.create({ color: data.color, x: data.x, y: data.y })
+                }
+            })()
+        }
+        if (action === "join") {
+            ws.clients.forEach(client => {
+                if (client.readyState == webSocket.OPEN)
+                    client.send(JSON.stringify({ action: "msg", data: { author: data.pseudo, content: "viens de se connecter !", type: 2 } }))
+            })
+        }
+    });
 });
 
 
